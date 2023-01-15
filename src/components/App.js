6b5295bc2e0327329eceede9b2b52e75832641f3
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-01-15 12:57:35 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
-// create your App component here
+
+import React from "react";
+import { useState, useEffect } from "react";
+
+// fetch data from API  
+function App() {
+  const[dog, setDog] =useState([]);
+  const[loadDog, setLoadDog] = useState(false)
+
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then(data => {
+            setDog(data);
+            setLoadDog(true);
+          })
+        }, [])
+
+        if(!loadDog){return <p>Loading...</p>}
+
+        return (
+            <div>
+                <img src={dog.message} alt="A Random Dog"></img>
+            </div>
+        )
+}
+
+export default App;
